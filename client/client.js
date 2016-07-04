9b30bbf4bf603993dc1dabdd3c05e62ba8c295bb
Christian Ambriz <christian.ambriz@bloc.io>
2016-07-04 15:43:27 -0700

    get react render component to show

--- client/client.js
+++ client/client.js
@@ -1,1 +1,11 @@
+import React from 'react'
+import { render } from 'react-dom'
+import App from '../components/App'
+
+render(
+  // definte the encompassing component,
+  // DOM element we want to mount it to.
+  <App/>,
+  document.getElementById('app')
+)
 
