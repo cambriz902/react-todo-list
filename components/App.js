ceefebbb9b16f3a25716e1f5b0deec06cd775f6c
Christian Ambriz <christian.ambriz@bloc.io>
2016-07-04 23:39:07 -0700

    add hot loading

--- components/App.js
+++ components/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 // var App = React.createClass({
 class App extends Component {
   render () {
-    return <div>This is definitely a React app now!</div>
+    return <div>This is definitely a hot (module reloading) React app now!</div>
   }
 }
 
