ce1a35c623637441960dcbc8732de33b7f85e8b9
Christian Ambriz <christian.ambriz@bloc.io>
2016-07-16 16:04:21 -0700

    added button to child that can delete text from parent

--- components/TextDisplay.js
+++ components/TextDisplay.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react'
 
 class TextDisplay extends Component {
 
-
+	handleClick() {
+		this.props.deleteLetter()
+	}
+	
   render() {
-    return <div>Our text from my parent: {this.props.text}</div>
+    return (
+    	<div>
+    		<div>Our text from my parent: {this.props.text}</div>
+    		<button onClick={this.handleClick.bind(this)}>delete one letter</button>
+      </div>
+    )
   }
 
 }
