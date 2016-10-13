a6a3faea66ae126dac26841c2de59d02ca1d51f1
christian <christian.ambriz@gmail.com>
2016-10-12 21:40:58 -0700

    update files

--- server/server.js
+++ server/server.js
@@ -14,11 +14,11 @@ app.use(webpackHotMiddleware(compiler));
 
 app.use(express.static('./dist'));
 
-app.use('/', function (req, res) {
+app.get('/', function (req, res) {
     res.sendFile(path.resolve('client/index.html'));
 });
 
-var port = 4000;
+var port = process.env.PORT || 4000;
 
 app.listen(port, function(error) {
   if (error) throw error;
