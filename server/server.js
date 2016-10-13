ebc41da841092a192da6e1f9e00cc077cd976615
christian <christian.ambriz@gmail.com>
2016-10-12 21:33:48 -0700

    update files

--- server/server.js
+++ server/server.js
@@ -1,37 +1,24 @@
 var express = require('express');
 var path = require('path');
-var config = require('../webpack.config.js');
+var config = require('../webpack.config.js')
 var webpack = require('webpack');
 var webpackDevMiddleware = require('webpack-dev-middleware');
 var webpackHotMiddleware = require('webpack-hot-middleware');
 
 var app = express();
-var compiler = webpack(config);
-
-app.use(webpackDevMiddleware(compiler, {noInfo: true, publicPath: config.output.publicPath}));
-app.use(webpackHotMiddleware(compiler));
 
 var compiler = webpack(config);
 
 app.use(webpackDevMiddleware(compiler, {noInfo: true, publicPath: config.output.publicPath}));
 app.use(webpackHotMiddleware(compiler));
-
-// set the view engine to ejs
-// app.set('client engine', 'js');
 
 app.use(express.static('./dist'));
 
-// app.use('/', function (req, res) {
-//     res.sendFile(path.resolve('client/index.html'));
-// });
-
-app.get('/', function(req, res) {
-
-    // ejs render automatically looks in the views folder
-    res.render(path.resolve('client/index.html'));
+app.use('/', function (req, res) {
+    res.sendFile(path.resolve('client/index.html'));
 });
 
-var port = process.env.PORT || 4000;
+var port = 3000;
 
 app.listen(port, function(error) {
   if (error) throw error;
