28f936506693c493be2ee30ce2afc7c580c3f16c
christian <christian.ambriz@gmail.com>
2016-10-12 21:17:05 -0700

    allow heroku to set port

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ app.use('/', function (req, res) {
     res.sendFile(path.resolve('client/index.html'));
 });
 
-var port = 4000;
+var port = process.env.PORT 4000;
 
 app.listen(port, function(error) {
   if (error) throw error;
